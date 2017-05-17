388084e1b32bd7ad8e47aba9cb49ed3218cd1c00
devico <clamdm@mail.ru>
2017-05-17 13:49:29 +0300

    fixed work engine with trainer_controller

--- app/controllers/api_promptcards/trainer_controller.rb
+++ app/controllers/api_promptcards/trainer_controller.rb
@@ -3,34 +3,35 @@ require_dependency "api_promptcards/application_controller"
 module ApiPromptcards
   class TrainerController < ApplicationController
     def index
-      # result = TrainingCard.call(user: current_user, card_id: params[:id])
-      # @card = result.card
-      
-      # respond_to do |format|
-      #   format.html
-      #   format.js
-      # end
+      result = ApiPromptcards::TrainingCard.call(user: current_user, card_id: params[:id])
+      @card = result.card
+    
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
 
-    # def review_card
-    #   @result = ReviewCard.call(
-    #     card_id: current_user.cards.find(params[:card_id]),
-    #     user_translation: trainer_params[:user_translation]
-    #   )
+    def review_card
+      @result = ApiPromptcards::ReviewCard.call(
+        card_id: current_user.cards.find(params[:card_id]),
+        user_translation: trainer_params[:user_translation], 
+        authenticity_token: trainer_params[:authenticity_token]
+      )
 
-    #   if @result.state
-    #     flash[:notice] = @result.message
-    #     redirect_to trainer_path
-    #   else
-    #     flash[:alert] = t('.incorrect_translation_alert')
-    #     redirect_to trainer_path(id: @result.card.id)
-    #   end
-    # end
+      if @result.state
+        flash[:notice] = @result.message
+        redirect_to trainer_path
+      else
+        flash[:alert] = t('.incorrect_translation_alert')
+        redirect_to trainer_path(id: @result.card.id)
+      end
+    end
 
-    # private
+    private
 
-    # def trainer_params
-    #   params.permit(:user_translation)
-    # end
-  end
+    def trainer_params
+      params.permit(:user_translation, :authenticity_token)
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 ApiPromptcards::Engine.routes.draw do
   root 'trainer#index'
+
+  put 'review_card' => 'trainer#review_card'
+  get 'trainer' => 'trainer#index'
+
 end
